4285d62a018ce37a817da70a6dc61c03b23f4bbc
leandroarnoso <lean089@hotmail.com>
2016-10-11 14:50:01 -0300

    TP 2

---
+++ TP 2/Alumno_Clase_12/Clase_12_Library/Camion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_12_Library_2;
+
+namespace Clase_12_Library
+{
+    public class Camion : Vehiculo
+    {
+        #region "Constructores"
+
+        public Camion(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        #endregion
+
+        #region "Propiedades"
+
+        /// <summary>
+        /// Los camiones tienen 8 ruedas
+        /// </summary>
+        protected override short CantidadRuedas
+        {
+            get
+            {
+                return 8;
+            }
+            set { }
+        }
+        #endregion
+
+        #region "Métodos"
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CAMION");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("RUEDAS : {0}", this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ TP 2/Alumno_Clase_12/Clase_12_Library/Moto.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clase_12_Library_2;
+
+using System.Drawing;
+
+namespace Clase_12_Library
+{
+    public class Moto : Vehiculo
+    {
+        #region "Constructores"
+
+        public Moto(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        #endregion
+
+        #region "Propiedades"
+
+        /// <summary>
+        /// Las motos tienen 2 ruedas
+        /// </summary>
+        protected override short CantidadRuedas
+        {
+            get
+            {
+                return 2;
+            }
+            set { }
+        }
+        #endregion
+
+        #region "Métodos"
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("MOTO");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("RUEDAS : {0}", this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
