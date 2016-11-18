80e62c1fb61961e752c9382ea51473d11f47f204
leandroarnoso <lean089@hotmail.com>
2016-11-18 15:41:36 -0300

    TP 4

---
+++ TP 4/Navegador TP-4 - AlumnoV2/Archivos/Texto.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        #region Atributos
+
+        private string _archivo;
+        #endregion
+
+        #region Constructores
+
+        public Texto(string archivo)
+        {
+            this._archivo = archivo;
+        }
+        #endregion
+
+        #region Metodos
+
+        public bool guardar(string datos)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(this._archivo, true, Encoding.UTF8))
+                {
+                    sw.WriteLine(datos);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool leer(out List<string> datos)
+        {
+            datos = new List<string>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(this._archivo, Encoding.UTF8))
+                {
+                    while (!sr.EndOfStream)
+                        datos.Add(sr.ReadLine());
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
---
+++ TP 4/Navegador TP-4 - AlumnoV2/Hilo/Descargador.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            this.html = "";
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public delegate void DownloadProgressChanged(int progreso);
+        public event DownloadProgressChanged ProgresoDescarga;
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            try
+            {
+                this.ProgresoDescarga(e.ProgressPercentage);
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+
+        public delegate void DownloadCompleted(string html);
+        public event DownloadCompleted DescargaCompletada;
+
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                this.html = e.Result;
+                this.DescargaCompletada(this.html);
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+    }
+}
